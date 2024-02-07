9da9114f2ef245613bcd405ae3878190e3cca745
avinashraj-151 <avinashrajraj628@gmail.con>
2024-02-07 06:17:09 +0530

    create pages and routes to this mernstack blog

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Signin from "./pages/Signin";
+import Singup from "./pages/Singup";
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
-    <div className='text-3xl bg-red-800'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/project" element={<Project />}></Route>
+        <Route path="/Sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<Singup />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
